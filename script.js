1ee09bdf0476e2f3bd1a5ea95de8888416b8d115
Shanel Hickman-Davis <shanelhickmandavis@gmail.com>
2022-09-20 12:35:33 -0400

    CSS modification
    CSS modification

--- script.js
+++ script.js
@@ -9,10 +9,21 @@ const computerScoreElem = document.getElementById("computer-score");
 const newBtn = document.getElementById("new-button");
 const modeBtn = document.getElementById("mode-button");
 const quitBtn = document.getElementById("quit-button");
+const winnerAlert = document.getElementById("winner-alert");
 
+let winner;
+let chooseWinner = () => {
+  if(playerScoreElem.textContent > computerScoreElem.textContent){ 
+   winner = 'Player'
+  } if(playerScoreElem.textContent < computerScoreElem.textContent){
+    winner = 'Computer'
+} else{
+    winner = 'Tied. No one '}
+}
 let prevTime;
+let gameOn = true;
 function update(time) {
-  if (prevTime != null) {
+  if (prevTime != null && gameOn) {
     const delta = time - prevTime;
     ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
 
@@ -24,7 +35,14 @@ function update(time) {
     document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
 
     if (isLost()) handleLoss();
-  }
+  } else if( gameOn == false){
+    const delta = 0;
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+        computerPaddle.update(delta, ball.y);
+        chooseWinner();
+        winnerAlert.textContent= `${winner} wins!`;
+        
+  } 
 
   prevTime = time;
   window.requestAnimationFrame(update);
@@ -46,25 +64,27 @@ function handleLoss() {
   computerPaddle.reset();
 }
 
-const reload = () => {};
+const reload = () => window.location.reload();
 
 const changeMode = () => {
-  console.log('changing difficulty mode');
+  console.log('changing mode');
   const initialText = 'Easy';
   if (modeBtn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
     modeBtn.innerHTML = '<span style="font-weight: bold">Hard</span>';
     playerPaddle.height -= 10;
   } else {
-    modeBtn.innerHTML = '<span style="font-weight: thin">initialText</span>';    playerPaddle.height += 10;
+    modeBtn.innerHTML = '<span style="font-weight: thin">Easy</span>';    playerPaddle.height += 10;
   }
   };
 
-const quit = () => {};
+const endGame = () => {
+  console.log('ending game') 
+  gameOn = false;}
 
 document.addEventListener("mousemove", (e) => {
   playerPaddle.position = (e.y / window.innerHeight) * 100;
 });
-newBtn.addEventListener("click", reload());
-modeBtn.addEventListener("click", changeMode());
-quitBtn.addEventListener("click", ()=>{/*make it stop*/});
+newBtn.addEventListener("click", reload);
+modeBtn.addEventListener("click", changeMode);
+quitBtn.addEventListener("click", endGame);
 window.requestAnimationFrame(update);
